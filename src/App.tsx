271aae1b93907b7300a5bda073298ebd54ccd66a
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-09 16:46:44 +0530

    Adding React Refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles.css'
 import IMAGE from  './react.png'
 import LOGO from  './logo.svg'
+import ClickCounter from './ClickCounter';
 function App() {
   return <>
-  <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
+  <h1>Edited React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
   <img src={IMAGE} alt="React Logo" width="300" height="200" />
   <img src={LOGO} alt="React Logo" width="300" />
+  <ClickCounter/>
   </>;
 }
 
