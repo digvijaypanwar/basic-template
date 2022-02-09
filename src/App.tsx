5e68172a3bf7cd5bba7df21bde9af72fac4a8b0d
Digvijay Panwar <panwardigvijay17@gmail.com>
2022-02-09 16:32:20 +0530

    adding different envs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import IMAGE from  './react.png'
 import LOGO from  './logo.svg'
 function App() {
   return <>
-  <h1>React TypeScript Webpack Starter Template</h1>
+  <h1>React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}</h1>
   <img src={IMAGE} alt="React Logo" width="300" height="200" />
   <img src={LOGO} alt="React Logo" width="300" />
   </>;
